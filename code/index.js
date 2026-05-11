1a3f47d4b43ee13237cb49bf517e9c2e7dd764ea
belugaman9981 <littledage@outlook.com>
2026-05-10 20:49:05 -0700

    Update index.js

--- code/index.js
+++ code/index.js
@@ -9,3 +9,12 @@ multi-line comment */
 /* Data Types: 
 undefined, null, boolean, string, symbol, number, and object
 */
+
+
+var name = "matt..."
+
+name = 8
+
+let samy = "sam..."
+
+const pi = 3.14
