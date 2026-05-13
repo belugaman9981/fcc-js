4d97f49bc0a5c1b443978b7fb544e80d4decb9f9
belugaman9981 <littledage@outlook.com>
2026-05-12 18:38:20 -0700

    Update numberTheory.js

--- numberTheory.js
+++ numberTheory.js
@@ -1,7 +1,17 @@
-const readline = require()
+const readline = require();
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
+
+rl.question("Enter a number: ", function(number) {
+  if (number % 2 == 0) {
+    console.log("Your Number is even.");
+
+  } else {
+    console.log("The number is odd.")
+  }
+  rl.close();
+});
 
